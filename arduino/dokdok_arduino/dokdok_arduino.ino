c7ba323b65ea17ae33a55e40ad68d45bcaf24f80
최민재 <cmjcmj1230@gmail.com>
2022-12-26 15:25:03 +0900

    ⚡️ :: Add Sample arduino code

---
+++ arduino/dokdok_arduino/dokdok_arduino.ino
@@ -0,0 +1,31 @@
+#define TRIG 9 // TRIG 핀 설정(초음파를 보내는 핀)
+#define ECHO 8 // ECHO 핀 설정(초음파를 받는 핀)
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+
+  duration = pulseIn(ECHO, HIGH); // 물체에 반사되어 돌아온 초음파의 시간을 변수에 저장한다.
+
+  // 34000 * 초음파가 물체로 부터 반사되어 돌아오는 시간 / 1000000 / 2 (왕복값이 아니라 편도값으로 나누기 2를 해준다.)
+  // 초음파 센서의 가리값이 위 계산값과 동일하게 Cm로 환산되는 계산공식. 수식이 간단해지도록 적용.
+  distance = duration * 17 / 1000;
+
+  Serial.println(duration); // 초음파가 반사되어 돌아오는 시간.
+  Serial.print("\nDistance : ");
+  Serial.print(distance); // 측정된 물체로부터 거리값(cm값)을 보여준다.
+  Serial.println("Cm");
+
+  delay(1000); // 1초마다 측정값을 보여준다.
+}
