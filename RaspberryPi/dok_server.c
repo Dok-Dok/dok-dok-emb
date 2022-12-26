142f242903e10beb6c8cad9acfd73e2cb667dd64
snack655 <cmjcmj@gmail.com>
2022-12-26 22:16:50 +0900

    Add : 문 상태 변경 파악 로직

--- RaspberryPi/dok_server.c
+++ RaspberryPi/dok_server.c
@@ -10,6 +10,9 @@
 char device[] = "/dev/ttyACM0";
 
 int serial;
+int isOpen = 0; // 0: close / 1: open 
+int count = 0;
+char str[10];
 unsigned long baud = 9600;
 unsigned long time = 0;
 
@@ -35,7 +38,20 @@ void setup() {
 void loop() {
 	if(serialDataAvail(serial)) {
 		char newChar = serialGetchar(serial);
-		printf("%c", newChar);
+		if(newChar != '\n') {
+			str[count++] = newChar;
+		} else {
+			count = 0;
+			if(atoi(str) > 15) {
+				printf("%d open\n", atoi(str));
+			} else {
+				printf("%d close\n", atoi(str));
+			}
+			for(int i = 0; i < 10; i++) {
+				str[i] = '\0';
+			}
+		}
+		//printf("%c", newChar);
 		fflush(stdout);
 	}
 }
